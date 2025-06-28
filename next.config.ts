6b1a53167d2b149dbe71ee01a8f2324064aa04f4
Subhash Bose <pythonlab@subhashbose.com>
2025-06-28 20:17:35 +0000

    after deploying to github pages the /_next/ assets path not found. can y

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || '';
+  assetPrefix = `/${repo}`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig: NextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
